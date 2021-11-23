69e9f0343c1380a2e39cc3dc659846e549e1caba
zullum <zullumsan@gmail.com>
2021-11-23 08:58:28 +0100

    Modifed modal screen for updating user data.

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,7 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore";
 import React, { useState } from "react";
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Button,
+} from "react-native";
 import tw from "tailwind-rn";
 import { db } from "../firebase";
 import useAuth from "../hooks/useAuth";
@@ -33,62 +44,71 @@ const ModalScreen = () => {
   };
 
   return (
-    <View style={tw("flex-1 items-center pt-5 bg-white")}>
-      <Image
-        style={tw("h-20 w-full")}
-        resizeMode="contain"
-        source={{ uri: "https://links.papareact.com/2pf" }}
-      />
-      <Text style={tw("text-xl text-gray-500 p-2 font-bold")}>
-        Welcome {user.displayName}
-      </Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={tw("flex-1")}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={tw("flex-1 items-center pt-5 bg-white relative")}>
+          <Image
+            style={tw("h-20 w-full")}
+            resizeMode="contain"
+            source={{ uri: "https://links.papareact.com/2pf" }}
+          />
+          <Text style={tw("text-xl text-gray-500 p-2 font-bold")}>
+            Welcome {user.displayName}
+          </Text>
 
-      <Text style={tw("text-center p-4 font-bold text-red-400")}>
-        Step 1. The Profile Pic
-      </Text>
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+            Step 1. The Profile Pic
+          </Text>
 
-      <TextInput
-        value={image}
-        onChangeText={setImage}
-        placeholder="Enter a Profile Pic URL"
-        style={tw("text-center text-xl pb-2")}
-      />
+          <TextInput
+            value={image}
+            onChangeText={setImage}
+            placeholder="Enter a Profile Pic URL"
+            style={tw("text-center text-xl pb-2")}
+          />
 
-      <Text style={tw("text-center p-4 font-bold text-red-400")}>
-        Step 2. The Occupation
-      </Text>
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+            Step 2. The Occupation
+          </Text>
 
-      <TextInput
-        value={job}
-        onChangeText={setJob}
-        placeholder="Enter you occupation"
-        style={tw("text-center text-xl pb-2")}
-      />
+          <TextInput
+            value={job}
+            onChangeText={setJob}
+            placeholder="Enter you occupation"
+            style={tw("text-center text-xl pb-2")}
+          />
 
-      <Text style={tw("text-center p-4 font-bold text-red-400")}>
-        Step 3. The Age
-      </Text>
+          <Text style={tw("text-center p-4 font-bold text-red-400")}>
+            Step 3. The Age
+          </Text>
 
-      <TextInput
-        value={age}
-        onChangeText={setAge}
-        placeholder="Enter your age"
-        style={tw("text-center text-xl pb-2")}
-        maxLength={2}
-        keyboardType="numeric"
-      />
+          <TextInput
+            value={age}
+            onChangeText={setAge}
+            placeholder="Enter your age"
+            style={tw("text-center text-xl pb-2")}
+            maxLength={2}
+            keyboardType="numeric"
+          />
 
-      <TouchableOpacity
-        onPress={updateUserProfile}
-        disabled={incompleteForm}
-        style={[
-          tw("w-64 p-3 rounded-xl absolute bottom-10"),
-          incompleteForm ? tw("bg-gray-400") : tw("bg-red-400"),
-        ]}
-      >
-        <Text style={tw("text-center text-white text-xl")}>Update Profile</Text>
-      </TouchableOpacity>
-    </View>
+          <TouchableOpacity
+            onPress={updateUserProfile}
+            disabled={incompleteForm}
+            style={[
+              tw("w-64 p-3 rounded-xl mt-10"),
+              incompleteForm ? tw("bg-gray-400") : tw("bg-red-400"),
+            ]}
+          >
+            <Text style={tw("text-center text-white text-xl")}>
+              Update Profile
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
